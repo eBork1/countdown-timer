4b9262e57b29c7264f198b70408194131048282e
Ethan Borkosky <ethan.borkosky@gmail.com>
2019-09-11 16:03:25 -0400

    update main.js

--- main.js
+++ main.js
@@ -7,10 +7,9 @@ var countdown = setInterval(function() {
     //conversions
     var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
     var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeLeft % (1000 * 60 )) / (1000 * 60));
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-    //
-  
+    
     if (seconds < 10) {
       seconds = "0" + seconds;
     }
@@ -27,11 +26,13 @@ var countdown = setInterval(function() {
       days = "0" + days;
     }
   
-    document.getElementById('time').innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
+    document.getElementById("time").innerHTML = days + ":" + hours + ":" + minutes + ":" + seconds;
     
-}, 1000);
 
-if (timeLeft < 0) {
-clearInterval(countdown);
-document.getElementById("time").innerHTML = "HAPPY NEW YEAR!";
-}
+
+    if (timeLeft < 0) {
+      clearInterval(countdown);
+      document.getElementById("time").innerHTML = "HAPPY NEW YEAR!";
+    }
+
+}, 1000);
